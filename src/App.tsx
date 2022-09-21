2e7fc927bb42d45b7e352ff958325087b16be1ad
Medo <martin.mederly@ydistri.com>
2022-09-21 08:07:44 +0200

    router v6 + docker

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import Root, { loader as rootLoader, action as rootAction } from "./routes/Root";
+import ErrorPage from "./ErrorPage";
+import Contact, { loader as contactLoader, action as contactAction } from "./routes/Contact";
+import EditContact, { action as editAction} from "./routes/Edit";
+import { action as destroyAction} from "./routes/Destroy";
+import Index from "./routes";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route
+            path="/"
+            element={<Root />}
+            errorElement={<ErrorPage />}
+            loader={rootLoader}
+        >
+            <Route
+                action={rootAction}
+                errorElement={<ErrorPage />}
+            >
+                <Route
+                    index
+                    element={<Index />}
+                />
+                <Route
+                    path="contacts/:contactId"
+                    element={<Contact />}
+                    loader={contactLoader}
+                    action={contactAction}
+                >
+                </Route>
+                <Route
+                    path="contacts/:contactId/edit"
+                    element={<EditContact />}
+                    loader={contactLoader}
+                    action={editAction}
+                >
+                </Route>
+                <Route
+                    path="contacts/:contactId/destroy"
+                    element={null}
+                    action={destroyAction}
+                    // errorElement={<div>Oops! There was an error.</div>}
+                >
+                </Route>
+            </Route>
+        </Route>
+    )
+)
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+  )
 }
 
-export default App;
+export default App
