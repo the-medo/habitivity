39bb0c0116f0ecf128d28f26e3c46e1d0dde9bc3
Medo <martinmederly@gmail.com>
2023-02-08 21:09:12 +0100

    style: calendar and its content styling

--- src/screens/Calendar/CalendarDefault.tsx
+++ src/screens/Calendar/CalendarDefault.tsx
@@ -1,23 +1,71 @@
 import React, { useCallback, useEffect } from 'react';
-import { DashboardSubpage } from './dashboardSlice';
-import { getItem, LSKey } from '../../store/localStore';
-import { useNavigate } from 'react-router-dom';
 import { Calendar } from 'antd';
 import dayjs, { Dayjs } from 'dayjs';
-import { PointCircle } from '../../components/global/PointCircle';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 import { ReduxState } from '../../store';
 import { setDateRange } from '../screenSlice';
 import { useGetCompletedDaysQuery } from '../../apis/apiTasks';
 import { useSelectedTaskListId } from '../../hooks/useSelectedTaskListId';
+import { COLORS } from '../../styles/CustomStyles';
+import { PointCircle } from '../../components/global/PointCircle';
 
 const CalendarStyled = styled(Calendar)`
+  .ant-picker-cell {
+    transition: all 0.3s ease-in-out;
+
+    .ant-picker-cell-inner {
+      transition: all 0.3s ease-in-out;
+      margin: 0;
+      border-left: 2px solid transparent !important;
+      border-right: 2px solid transparent !important;
+      border-bottom: 2px solid transparent !important;
+    }
+
+    &.ant-picker-cell-selected {
+      transition: all 2s ease-in-out;
+
+      .ant-picker-calendar-date-value {
+        font-weight: 500;
+      }
+
+      .ant-picker-cell-inner {
+        border: 2px solid ${COLORS.PRIMARY} !important;
+      }
+    }
+  }
+
   .ant-picker-calendar-date-content {
     display: flex;
-    //align-items: center;
+    align-items: center;
     justify-content: center;
+    height: 3.5rem !important;
+    transform: translateY(-20%);
   }
+`;
+
+const AllWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+`;
+
+const CalendarWrapper = styled.div`
+  display: flex;
+  flex-basis: 40rem;
+`;
+
+const DayOverviewWrapper = styled.div`
+  display: flex;
+  flex-basis: 20rem;
+  flex-grow: 1;
+  border: 1px solid black;
+`;
+
+const CalendarDayPoints = styled(PointCircle)`
+  font-size: 1rem;
+  font-weight: 500;
 `;
 
 const CalendarDefault: React.FC = () => {
@@ -47,19 +95,31 @@ const CalendarDefault: React.FC = () => {
         const completedDay = monthlyData[value.format('YYYY-MM-DD')];
         if (!completedDay) return null;
 
-        console.log(completedDay.taskLists?.[selectedTaskListId]);
+        console.log(completedDay.taskLists[selectedTaskListId]);
 
         return (
-          <PointCircle $size="ultra">
+          <CalendarDayPoints
+            $size="ultra"
+            $color="rgba(255, 255, 255, .5)"
+            $colorText={COLORS.PRIMARY_DARK}
+            $mode="dark"
+          >
             {completedDay ? completedDay.taskLists[selectedTaskListId].toFixed(2) : '-'}
-          </PointCircle>
+          </CalendarDayPoints>
         );
       }
     },
     [selectedDay, monthlyData, selectedTaskListId],
   );
 
-  return <CalendarStyled dateCellRender={dateCellRender} />;
+  return (
+    <AllWrapper>
+      <CalendarWrapper>
+        <CalendarStyled dateCellRender={dateCellRender} />
+      </CalendarWrapper>
+      <DayOverviewWrapper>Day Overview</DayOverviewWrapper>
+    </AllWrapper>
+  );
 };
 
 export default CalendarDefault;
