a4c9c915310b41f506635a269ea81f6f9afd5bcc
Medo <martinmederly@gmail.com>
2023-02-15 22:19:00 +0100

    feat: lexical - typescript

--- src/components/global/Editor/plugins/CodeHighlightPlugin/CodeHighlightPlugin.tsx
+++ src/components/global/Editor/plugins/CodeHighlightPlugin/CodeHighlightPlugin.tsx
@@ -2,10 +2,14 @@ import { registerCodeHighlighting } from '@lexical/code';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { useEffect } from 'react';
 
-export default function CodeHighlightPlugin() {
+const CodeHighlightPlugin = (): null => {
   const [editor] = useLexicalComposerContext();
+
   useEffect(() => {
     return registerCodeHighlighting(editor);
   }, [editor]);
+
   return null;
-}
+};
+
+export default CodeHighlightPlugin;
---
+++ src/components/global/Editor/plugins/ToolbarPlugin/BlockOptionsDropdownList.tsx
@@ -0,0 +1,183 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+import { $getSelection, $isRangeSelection, $createParagraphNode, LexicalEditor } from 'lexical';
+import { $wrapNodes } from '@lexical/selection';
+import {
+  INSERT_ORDERED_LIST_COMMAND,
+  INSERT_UNORDERED_LIST_COMMAND,
+  REMOVE_LIST_COMMAND,
+} from '@lexical/list';
+import { $createHeadingNode, $createQuoteNode } from '@lexical/rich-text';
+import { $createCodeNode } from '@lexical/code';
+
+interface BlockOptionsDropdownListProps {
+  editor: LexicalEditor;
+  blockType: string;
+  toolbarRef: React.RefObject<HTMLDivElement>;
+  setShowBlockOptionsDropDown: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const BlockOptionsDropdownList = ({
+  editor,
+  blockType,
+  toolbarRef,
+  setShowBlockOptionsDropDown,
+}: BlockOptionsDropdownListProps): JSX.Element => {
+  const dropDownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const toolbar = toolbarRef.current;
+    const dropDown = dropDownRef.current;
+
+    if (toolbar !== null && dropDown !== null) {
+      const { top, left } = toolbar.getBoundingClientRect();
+      dropDown.style.top = `${top + 40}px`;
+      dropDown.style.left = `${left}px`;
+    }
+  }, [dropDownRef, toolbarRef]);
+
+  useEffect(() => {
+    const dropDown = dropDownRef.current;
+    const toolbar = toolbarRef.current;
+
+    if (dropDown !== null && toolbar !== null) {
+      const handle = (event: MouseEvent) => {
+        // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+        const target = event.target as Node;
+
+        if (!dropDown.contains(target) && !toolbar.contains(target)) {
+          setShowBlockOptionsDropDown(false);
+        }
+      };
+      document.addEventListener('click', handle);
+
+      return () => {
+        document.removeEventListener('click', handle);
+      };
+    }
+  }, [dropDownRef, setShowBlockOptionsDropDown, toolbarRef]);
+
+  const formatParagraph = useCallback(() => {
+    if (blockType !== 'paragraph') {
+      editor.update(() => {
+        const selection = $getSelection();
+
+        if ($isRangeSelection(selection)) {
+          $wrapNodes(selection, () => $createParagraphNode());
+        }
+      });
+    }
+    setShowBlockOptionsDropDown(false);
+  }, [blockType, editor, setShowBlockOptionsDropDown]);
+
+  const formatLargeHeading = useCallback(() => {
+    if (blockType !== 'h1') {
+      editor.update(() => {
+        const selection = $getSelection();
+
+        if ($isRangeSelection(selection)) {
+          $wrapNodes(selection, () => $createHeadingNode('h1'));
+        }
+      });
+    }
+    setShowBlockOptionsDropDown(false);
+  }, [blockType, editor, setShowBlockOptionsDropDown]);
+
+  const formatSmallHeading = useCallback(() => {
+    if (blockType !== 'h2') {
+      editor.update(() => {
+        const selection = $getSelection();
+
+        if ($isRangeSelection(selection)) {
+          $wrapNodes(selection, () => $createHeadingNode('h2'));
+        }
+      });
+    }
+    setShowBlockOptionsDropDown(false);
+  }, [blockType, editor, setShowBlockOptionsDropDown]);
+
+  const formatBulletList = useCallback(() => {
+    if (blockType !== 'ul') {
+      editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND, undefined);
+    } else {
+      editor.dispatchCommand(REMOVE_LIST_COMMAND, undefined);
+    }
+    setShowBlockOptionsDropDown(false);
+  }, [blockType, editor, setShowBlockOptionsDropDown]);
+
+  const formatNumberedList = useCallback(() => {
+    if (blockType !== 'ol') {
+      editor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND, undefined);
+    } else {
+      editor.dispatchCommand(REMOVE_LIST_COMMAND, undefined);
+    }
+    setShowBlockOptionsDropDown(false);
+  }, [blockType, editor, setShowBlockOptionsDropDown]);
+
+  const formatQuote = useCallback(() => {
+    if (blockType !== 'quote') {
+      editor.update(() => {
+        const selection = $getSelection();
+
+        if ($isRangeSelection(selection)) {
+          $wrapNodes(selection, () => $createQuoteNode());
+        }
+      });
+    }
+    setShowBlockOptionsDropDown(false);
+  }, [blockType, editor, setShowBlockOptionsDropDown]);
+
+  const formatCode = useCallback(() => {
+    if (blockType !== 'code') {
+      editor.update(() => {
+        const selection = $getSelection();
+
+        if ($isRangeSelection(selection)) {
+          $wrapNodes(selection, () => $createCodeNode());
+        }
+      });
+    }
+    setShowBlockOptionsDropDown(false);
+  }, [blockType, editor, setShowBlockOptionsDropDown]);
+
+  return (
+    <div className="dropdown" ref={dropDownRef}>
+      <button className="item" onClick={formatParagraph}>
+        <span className="icon paragraph" />
+        <span className="text">Normal</span>
+        {blockType === 'paragraph' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatLargeHeading}>
+        <span className="icon large-heading" />
+        <span className="text">Large Heading</span>
+        {blockType === 'h1' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatSmallHeading}>
+        <span className="icon small-heading" />
+        <span className="text">Small Heading</span>
+        {blockType === 'h2' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatBulletList}>
+        <span className="icon bullet-list" />
+        <span className="text">Bullet List</span>
+        {blockType === 'ul' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatNumberedList}>
+        <span className="icon numbered-list" />
+        <span className="text">Numbered List</span>
+        {blockType === 'ol' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatQuote}>
+        <span className="icon quote" />
+        <span className="text">Quote</span>
+        {blockType === 'quote' && <span className="active" />}
+      </button>
+      <button className="item" onClick={formatCode}>
+        <span className="icon code" />
+        <span className="text">Code Block</span>
+        {blockType === 'code' && <span className="active" />}
+      </button>
+    </div>
+  );
+};
+
+export default BlockOptionsDropdownList;
