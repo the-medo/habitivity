42f19053bb40d62c22d332fb4f0fd7a74ea2d43c
Medo <martinmederly@gmail.com>
2023-02-17 17:37:43 +0100

    feat: extract html elements and their styles into styled components

---
+++ src/components/global/Editor/plugins/ToolbarPlugin/toolbarComponents.ts
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+interface ToolbarItemButtonProps {
+  $active?: boolean;
+}
+
+export const ToolbarItemButton = styled.button<ToolbarItemButtonProps>`
+  border: 0;
+  display: flex;
+  background: none;
+  border-radius: 10px;
+  padding: 8px;
+  cursor: pointer;
+  vertical-align: middle;
+  font-size: 18px;
+  gap: 0.25rem;
+
+  ${p => p.$active && 'background-color: rgba(223, 232, 250, 0.3);'}
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.2;
+  }
+
+  &:hover:not([disabled]) {
+    background-color: #eee;
+  }
+`;
+
+export const Divider = styled.div`
+  width: 1px;
+  background-color: #eee;
+  display: inline-block;
+`;
+
+export const Toolbar = styled.div`
+  display: flex;
+  margin-bottom: 1px;
+  background: #fff;
+  padding: 4px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  border-bottom: 1px solid #ccc;
+  vertical-align: middle;
+  gap: 2px;
+`;
+
+export const ToolbarSelect = styled.select`
+  border: 0;
+  display: flex;
+  background: none;
+  border-radius: 10px;
+  padding: 8px;
+  vertical-align: middle;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  width: 70px;
+  font-size: 14px;
+  color: #777;
+  text-overflow: ellipsis;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #eee;
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
